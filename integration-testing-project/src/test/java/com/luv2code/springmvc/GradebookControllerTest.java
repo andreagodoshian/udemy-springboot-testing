97575c79c66666ca7c3e1df1a4cdb3e8fd67ee6c
godoshian <94014358+godoshian@users.noreply.github.com>
2022-07-15 12:14:57 -0400

    test - memory db // app - mysql db

--- integration-testing-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ integration-testing-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -32,7 +32,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@TestPropertySource("/application.properties")
+@TestPropertySource("/application-test.properties")
 @AutoConfigureMockMvc
 @SpringBootTest
 public class GradebookControllerTest {
--- integration-testing-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ integration-testing-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@TestPropertySource("/application.properties")
+@TestPropertySource("/application-test.properties")
 @SpringBootTest
 public class StudentAndGradeServiceTest {
 
